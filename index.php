c792500426dea415e3caa7b8bd7b3f86e77361ef
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-01-01 14:28:16 +0100

    :sparkles: FRONT page identification v1

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <h1 class="text-center font-poppins font-semibold text-[20]">Bienvenue !</h1>
+
 
     <?php
     // echo date('y-m-d h:i:s');
@@ -58,23 +58,18 @@
 
 
     <form class="text-center" action="connexion.php" method="post">
-        <h2 class="text-[20] text-center">Connexion</h2>
-        <div class="form-group">
-            <input type="email" name="email" class="form-control" placeholder="Email" required>
+        <h2 class="mt-[150px] mb-[50px] text-center font-poppins font-semibold text-[20px]">S'identifier</h2>
+        <div class="pt-[22px] px-[22px] form-group">
+            <input type="email" name="email" class="p-1 px-16 py-2 rounded-[9px] border-2 form-control" placeholder="Email" required>
         </div>
-        <div class="form-group">
-            <input type="password" name="password" class="form-control" placeholder="Mot de passe" required>
+        <div class="p-[22px] form-group">
+            <input type="password" name="password" class="p-1 px-16 py-2 rounded-[9px] border-2 form-control" placeholder="Mot de passe" required>
         </div>
-        <p class="text-[12px]">Vous n'êtes pas encore inscrits ? <a class="text-[12px] font-poppins font-semibold " href="inscription.php">Cliquez sur ce lien</a></p>
+        <p class="mb-[22px] text-[12px]">Vous n'êtes pas encore inscrits ? <a class="text-[12px] font-poppins font-semibold " href="inscription.php">Cliquez sur ce lien</a></p>
         <div class="form-group">
-            <button class="text-white bg-gray-700 px-16 py-2 rounded-[9px]" type="submit">Connexion</button>
+            <button class="text-white bg-gray-700 px-16 py-2 rounded-[9px]" type="submit">S'identifier</button>
         </div>
     </form>
-
-
-
-
-
 
 </body>
 
