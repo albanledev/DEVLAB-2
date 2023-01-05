e2278d508655fc062febfdeab6e5d64014048ca1
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-01-05 12:53:31 +0100

    :sparkles: FRONT header + footer links

--- profil.php
+++ profil.php
@@ -29,7 +29,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 <body>
     <header>
         <div class="py-[20px] text-white  hidden place-content-around bg-gray-900 w-[100%] sm:flex">
-            <a class="flex hover:text-gray-300" href="#"> <img class="w-[25px] h-[25px]" src="img/profile.png" alt=""><p class="ml-[10px]">profil</p> </a>
+            <a class="flex hover:text-gray-300" href="profil.php"> <img class="w-[25px] h-[25px]" src="img/profile.png" alt=""><p class="ml-[10px]">profil</p> </a>
             <a class="flex hover:text-gray-300" href="landing.php"><img class="w-[25px] h-[25px]" src="img/home.png" alt=""><p class="ml-[10px]">accueil</p></a>
             <a class="flex hover:text-gray-300" href="#"><img class="w-[25px] h-[25px]" src="img/loupe%20(1).png" alt=""><p class="ml-[10px]">recherche</p></a>
             <a class="flex hover:text-gray-300" href="#"><img class="w-[22px] h-[25px]" src="img/invit.png" alt=""><p class="ml-[10px]">invitations</p></a>
@@ -79,7 +79,6 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 
         echo "<br><br>
-        <div class='block'>
         <div class=''>
         <div class='ml-[20px] font-poppins font-semibold text-[16px]'><h3> " . $album['name'] . "</h3></div>
         <p class='text-gray-400 ml-[20px] font-poppins text-[12px]'>" . $album['likes'] . " likes</p>
@@ -99,7 +98,7 @@ error_reporting(E_ALL & ~E_NOTICE);
             </div>
     
     
-            </form></div></div>
+            </form></div>
         ";
         }
 
