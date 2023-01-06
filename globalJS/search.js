1a0d46b70457a44d2d62778fcd235b0ab1ab005f
Alban de Braquilanges <alban2bqs@gmail.com>
2023-01-06 17:10:59 +0100

    dynamic searchbar fonctionnel

--- globalJS/search.js
+++ globalJS/search.js
@@ -1,71 +1,43 @@
-// const inputField = document.querySelector('#search-input');
 
-// inputField.addEventListener('input', () => {
-//   // Effectuez la recherche et affichez les résultats ici
-// });
+    
 
 
-// const axios = require('axios');
+    chercherMovie()
 
-// async function searchMovies(searchTerm) {
-//   const apiKey = 'your_api_key';
-//   const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`;
-//   try {
-//     const response = await axios.get(url);
-//     return response.data.results;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+    function chercherMovie(){
 
-// inputField.addEventListener('input', async () => {
-//   const searchResults = await searchMovies(inputField.value);
-//   // Affichez les résultats de la recherche ici
-// });
+        let str = document.querySelector("#searchbar");
+        let propositions = document.querySelector("#submenu");
 
+        str.addEventListener("keyup", function(){
+            if (str.value !== ""){
+                axios.get('https://api.themoviedb.org/3/search/movie?api_key=db5946f8d90a2a4716c7c2c3520a77b3&query='+str.value)
+    
+                    .then (response => {
+    
 
+                        console.log(response.data.results);
 
+                        let movie = response.data.results
+                        
+                        propositions.innerHTML = "";
+    
+                        for (let i = 0; i <= 4; i++) {
+                            let resultat = document.createElement("li");
+                            let myLink = "movie.php?id="+movie[i].id+"&name=" + movie[i].title + "&bin=" + movie[i]['poster_path']
+                            resultat.innerHTML = "<div class='flex'><img class='w-auto h-[50px]'src='https://www.themoviedb.org/t/p/w600_and_h900_bestv2"+ movie[i].poster_path+"' alt>"+ "<div>                    <p><a href="+myLink+">"+movie[i].original_title+"</a></p></div></div>"
+                            propositions.appendChild(resultat);
+                        }
+                    })
 
-
-
-find()
-
-function find(){
-    // Requêter un utilisateur avec un ID donné.
-
-    let str = document.querySelector("#searchbar");
-    let propositions = document.querySelector("#submenu");
-
-    if (str.value !== ""){
-        axios.get(/* '/user?ID=2' */ 'https://api.themoviedb.org/3/search/movie?api_key=db5946f8d90a2a4716c7c2c3520a77b3&query='+str.value)
-
-            .then(function (response) {
-                // en cas de réussite de la requête
-                console.log(response.data.results);
-                let movie = response.data.results
-
+            }else{
                 propositions.innerHTML = "";
+            }
+    
+        })
+        }
+    
+      
 
-                for (let i = 0; i <= 4; i++) {
-                    let result = document.createElement("li");
-                    let myLink = "single.php?id="+movie[i].id
-                    result.innerHTML = "<p><a href="+myLink+">"+movie[i].original_title+"</a></p>"
-                    propositions.appendChild(result);
-                }
-            })
 
-            .catch(function (error) {
-                // en cas d’échec de la requête
-                console.log(error);
-            })
-
-            .then(function () {
-                // dans tous les cas
-                console.log("AAAAAAAAA");
-            });
-    }else{
-        propositions.innerHTML = "";
-    }
-
-}
 
